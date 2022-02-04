ef64b0069ccff6e4742b857766c1583831d1f288
Angelo Fernandes Oliveira <afbo2003@gmail.com>
2022-02-04 15:48:22 -0300

    cria estrutura inicial, estilizacao da pag principal, configura service da api e inicia contextos

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/api';
+
+import styles from './styles.module.scss';
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
+
+export function Loginbox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c0a2759f9285ebedef10`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=');
+
+      // tirando o githubCode da url
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode);
+    }
+  }, []);
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com Github
+      </a>
+    </div>
+  );
+}
